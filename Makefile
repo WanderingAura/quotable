40d9b21ca688f9ba1c00d7f9322868a1105c5b59
Yangyang <jiachen.wang2000@gmail.com>
2024-03-19 21:15:50 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+run:
+	go run ./cmd/web
+psql:
+	psql ${QUOTABLE_DB_DSN}
+up:
+	@echo 'Running up migrations...'
+	migration -path ./migrations -database ${QUOTABLE_DB_DSN} up
---
+++ cmd/web/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() http.Handler {
+	router = httprouter.New()
+
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.MethodNotAllowedResponse)
+
+	router.HandlerFunc(http.MethodGet, "/", app.homeHandler)
+	router.HandlerFunc(http.MethodGet, "/discover", app.listQuoteHandler)
+	router.HandlerFunc(http.MethodGet, "/login", app.userLoginHandler)
+	router.HandlerFunc(http.MethodPost, "/login", app.userLoginPostHandler)
+	router.HandlerFunc(http.MethodGet, "/discover/:id", app.quoteHandler)
+
+	return router
+
+}
