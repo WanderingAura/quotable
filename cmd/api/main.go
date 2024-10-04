11c5831ce7766e159254eda1f45701291b6389e3
volcanoeyes <130888597+volcanoeyes@users.noreply.github.com>
2024-10-04 22:28:44 +0800

    fix dsn env var

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -71,7 +71,7 @@ func main() {
 	flag.StringVar(&config.logPath, "log-path", "./logs/quotable.log", "File to write error logs in")
 
 	// database configs
-	flag.StringVar(&config.db.dsn, "db-dsn", "", "Postgres database source name")
+	flag.StringVar(&config.db.dsn, "db-dsn", os.Getenv("QUOTABLE_DB_DSN"), "Postgres database source name")
 	flag.IntVar(&config.db.maxOpenConnections, "db-max-open-conns", 25, "Postgres max open connections")
 	flag.IntVar(&config.db.maxIdleConnections, "db-max-idle-conns", 25, "Postgres max idle connections")
 	flag.StringVar(&config.db.maxIdleDuration, "db-max-idle-time", "15m", "Postgres max connection idle time")
@@ -89,10 +89,6 @@ func main() {
 		fmt.Printf("Version:\t%s\n", version)
 		fmt.Printf("Build time:\t%s\n", buildTime)
 		os.Exit(0)
-	}
-
-	if config.db.dsn == "" {
-		config.db.dsn = os.Getenv("QUOTABLE_DSN")
 	}
 
 	logFile := os.Stderr
